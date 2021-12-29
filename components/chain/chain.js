220abb8c3ba8936be2b596e2f980deba1aad089a
PhucLa <laducphuc@gmail.com>
2021-12-29 11:16:16 +0700

    Update footer

--- components/chain/chain.js
+++ components/chain/chain.js
@@ -75,16 +75,17 @@ export default function Chain({ chain, isToken, chains }) {
       })
     } else {
       const params = {
-      chainId: toHex(chain.chainId), // A 0x-prefixed hexadecimal string
-      chainName: chain.name,
-      nativeCurrency: {
-        name: chain.nativeCurrency.name,
-        symbol: chain.nativeCurrency.symbol, // 2-6 characters long
-        decimals: chain.nativeCurrency.decimals,
-      },
-      rpcUrls: chain.rpc,
-      blockExplorerUrls: [ ((chain.explorers && chain.explorers.length > 0 && chain.explorers[0].url) ? chain.explorers[0].url : chain.infoURL) ]
-    }
+        chainId: toHex(chain.chainId), // A 0x-prefixed hexadecimal string
+        chainName: chain.name,
+        nativeCurrency: {
+          name: chain.nativeCurrency.name,
+          symbol: chain.nativeCurrency.symbol, // 2-6 characters long
+          decimals: chain.nativeCurrency.decimals,
+        },
+        rpcUrls: chain.rpc,
+        blockExplorerUrls: [ ((chain.explorers && chain.explorers.length > 0 && chain.explorers[0].url) ? chain.explorers[0].url : chain.infoURL) ]
+      }
+      
       window.web3.eth.getAccounts((error, accounts) => {
         window.ethereum.request({
           method: 'wallet_addEthereumChain',
